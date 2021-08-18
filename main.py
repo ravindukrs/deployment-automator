a18150e1666dd089417ef6fdb877f99c2abefff6
ravindukrs <ravindupf@gmail.com>
2021-08-18 19:25:40 +0530

    Added: Update Jmeter configuraition with Public IP of deployment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import subprocess
 import time
+import xml.etree.ElementTree as ET
+
+
 def get_front_end_ip():
     front_end_ip = subprocess.check_output(
         "cd deployment-config &&  kubectl get services --namespace=sock-shop |  awk '/front-end/ {print $4}'",
@@ -12,13 +15,26 @@ def get_front_end_ip():
         time.sleep(5)
         get_front_end_ip()
 
+def modify_xml(ip):
+    mytree = ET.parse('./deployment-config/jmeter-script.jmx')
+    myroot = mytree.getroot()
+    for item in myroot.iter('stringProp'):
+        if(item.attrib['name'] == 'HTTPSampler.domain'):
+            item.text = ip
 
+    mytree.write('./deployment-config/jmeter-script.jmx')
+    print("Updated JMX")
 
 if __name__ == '__main__':
+    # Deploy Microservice on AKS
     subprocess.call("chmod +x ./deployment-config/deployment-automater.sh", shell=True)
     subprocess.call("cd deployment-config && ./deployment-automater.sh", shell=True)
 
+    # Get Public IP of Front End
     print("Getting Public IP of Front End Service")
-    external_ip = get_front_end_ip()
-    print(external_ip)
+    external_ip = get_front_end_ip().strip()
+    print("External IP ", external_ip)
+
+    # Update Jmeter Configuration
+    modify_xml(external_ip)
 
